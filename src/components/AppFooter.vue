e0852ae713a4b63af36a9268b0fa5ff3211d1d74
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-04-14 01:39:05 +0200

    fix media-querry

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,7 +4,7 @@
 		<div class="bg-primary my-z">
 			<div class="my-market d-flex align-items-center justify-content-center ">
 				<ul class="d-flex justify-content-center  m-0">
-					<li v-for="(bar, index) in MarketPlace" :key="index" class="d-flex align-item-center flex-sm-column flex-md-row text-sm-center">
+					<li v-for="(bar, index) in MarketPlace" :key="index" class="d-flex align-item-center flex-column flex-md-row text-sm-center">
 						<div class="px-3 d-flex align-self-center">
 							<img :src="bar.img" :alt="bar.text">
 						</div>
@@ -20,10 +20,10 @@
 		<!--// market place  -->
 
 		<!-- main footer -->
-			<div class="my-body-footer d-flex justify-content-around px-5">
+			<div class="my-body-footer d-flex  justify-content-md-center justify-content-md-around px-sm-5">
 				<ul class="d-flex flex-column flex-wrap gap-1 m-0">
-					<li v-for="(footers, index) in ulFooter" :key="index">
-						<h3 class="text-white p-0 my-2">{{ footers.title }}</h3>
+					<li v-for="(footers, index) in ulFooter" :key="index" class="mx-4">
+						<h3 class="text-white p-0 my-3">{{ footers.title }}</h3>
 						<ul class="p-0">
 							<li v-for="(child, index2) in footers.list" :key="index2">
 								<p class="text-secondary m-0 p-0">{{ child }}</p>
@@ -31,9 +31,9 @@
 						</ul>
 					</li>
 				</ul>
-				<div>
+			
 					<img class="my-relative" src="../../public/img/dc-logo-bg.png" alt="">
-				</div>
+			
 			</div>
 		<!-- //main footer -->
 
@@ -49,11 +49,11 @@
 					<h4 class="text-uppercase text-primary">
 						follow us
 					</h4>
-					<img class="px-3  d-sm-none" src="../../public/img/footer-facebook.png" alt="">
-					<img class="px-1 d-sm-none" src="../../public/img/footer-twitter.png" alt="">
-					<img class="px-1 d-sm-none" src="../../public/img/footer-youtube.png" alt="">
-					<img class="px-1 d-sm-none" src="../../public/img/footer-pinterest.png" alt="">
-					<img class="px-1 d-sm-none" src="../../public/img/footer-periscope.png" alt="">
+					<img class="px-3  d-none d-md-block" src="../../public/img/footer-facebook.png" alt="">
+					<img class="px-1 d-none d-md-block" src="../../public/img/footer-twitter.png" alt="">
+					<img class="px-1 d-none d-md-block" src="../../public/img/footer-youtube.png" alt="">
+					<img class="px-1 d-none d-md-block" src="../../public/img/footer-pinterest.png" alt="">
+					<img class="px-1 d-none d-md-blomd" src="../../public/img/footer-periscope.png" alt="">
 				</div>
 			</div>
 		</div>
@@ -136,7 +136,8 @@ ul{
 	}
 
 	.my-relative{
-		scale: 70%;
+		scale: 30%;
+		bottom: 80px;
 	}
 }
 </style>
